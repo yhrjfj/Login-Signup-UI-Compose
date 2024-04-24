4eaeab355878cfe87723ea544ace6ecdab62a33d
RiJo <102893295+yhrjfj@users.noreply.github.com>
2024-04-24 19:32:50 +0600

    Add navigation dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,6 +50,8 @@ android {
 }
 
 dependencies {
+    //Navigation
+    implementation(libs.androidx.navigation.compose)
 
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
