4535fe246a8c3a0b09a56afb3a072ffa2375a81d
RiJo <102893295+yhrjfj@users.noreply.github.com>
2024-04-25 20:58:42 +0600

    Create navigation component

---
+++ app/src/main/java/com/yhr/jfj/ui/Navigation.kt
@@ -0,0 +1,41 @@
+package com.yhr.jfj.ui
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import com.yhr.jfj.ui.ui.HomeScreen
+import com.yhr.jfj.ui.ui.login.LoginScreen
+import com.yhr.jfj.ui.ui.signup.PolicyScreen
+import com.yhr.jfj.ui.ui.signup.PrivacyScreen
+import com.yhr.jfj.ui.ui.signup.SignUpScreen
+
+sealed class Route {
+    data class LoginScreen(val name: String = "login_screen") : Route()
+    data class SignUpScreen(val name: String = "sign_up_screen") : Route()
+    data class PrivacyScreen(val name: String = "privacy_screen") : Route()
+    data class PolicyScreen(val name: String = "policy_screen") : Route()
+    data class HomeScreen(val name: String = "home_screen") : Route()
+}
+
+@Composable
+fun Navigation(navController: NavHostController) {
+    NavHost(navController = navController, startDestination = Route.LoginScreen().name) {
+        composable(route = Route.LoginScreen().name) {
+            LoginScreen()
+        }
+        composable(route = Route.SignUpScreen().name) {
+            SignUpScreen()
+        }
+        composable(route = Route.PrivacyScreen().name) {
+            PrivacyScreen {}
+        }
+        composable(route = Route.PolicyScreen().name) {
+            PolicyScreen {}
+        }
+        composable(route = Route.HomeScreen().name) {
+            HomeScreen()
+        }
+
+    }
+}
